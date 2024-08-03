bb96a97a95bda5ab27eb59fff9ca2747c0156ce2
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-08-03 11:52:08 +0530

    Optimal Solution is added using Hashmap

--- LeetCode_560.cpp
+++ LeetCode_560.cpp
@@ -9,6 +9,11 @@ A subarray is a contiguous non-empty sequence of elements within an array.
 
 // Brute Solution 
 
+/*
+    Time Complexity: near to O(n^2)
+    Space Complexity: O(1)
+*/
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
@@ -22,6 +27,30 @@ public:
                 }
             }
         }
+        return count;
+    }
+};
+
+// Optimal Solution
+
+/*
+    Time Complexity: O(n logn)
+    Space Compexity: O(n)
+*/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int> mpp;
+        mpp[0] = 1;
+        int preSum = 0, count = 0;
+        for(int i = 0 ; i < nums.size() ; i++) {
+            preSum += nums[i];
+            int remove = preSum - k;
+            count += mpp[remove];
+            mpp[preSum] += 1;
+        }
+
         return count;
     }
 };
